80339921e79a06cc37ec98b5817a9bcb733f5bd3
markriley9999 <markriley9999@gmail.com>
2017-06-07 13:03:41 +0100

    Install

---
+++ install/getcontent-markriley9999-duk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./getcontent.sh "http://duk-test01.digitaluk.co.uk/markriley/refplayer-content/refplayer-content.tar.gz"
+
--- install/install-2.sh
+++ install/install-2.sh
@@ -2,7 +2,9 @@
 
 ./git-pull.sh
 ./node-setup.sh
-./getcontent-markriley9999-aws.sh
+
+./getcontent-markriley9999-duk.sh
+#./getcontent-markriley9999-aws.sh
 
 echo Install Complete
 
