1fe3907becd4efe31214b239ef1cc75fdc257d07
manufacturer <manufacturer@digitaluk.co.uk>
2017-06-07 16:07:36 +0100

    New install script

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-sudo dnf install git
+if ! [ -x "$(command -v git)" ]; then
+  echo 'First, installing git'
+  
+  if [ -x "$(command -v dnf)" ]; then
+    dnf install git
+  elif [ -x "$(command -v yum)" ]; then
+    yum install git
+  elif [ -x "$(command -v apt-get)" ]; then
+    apt-get install git
+  else
+    echo "Please first install git."
+    exit 1
+  fi
+
+fi
 
 
 git config --global user.name "manufacturer"
