ae58b2a032a292eecb7b0227a26f9d876c56dd78
markriley9999 <markriley9999@gmail.com>
2017-06-09 18:54:50 +0100

    new install file

---
+++ install/appinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APP=$1
+
+if ! [ -x "$(command -v $APP)" ]; then
+
+  echo Installing: $APP
+  
+  if [ -x "$(command -v dnf)" ]; then
+    sudo dnf install $APP
+  elif [ -x "$(command -v yum)" ]; then
+    sudo yum install $APP
+  elif [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get install $APP
+  else
+    echo Cannot install $APP
+    exit 1
+  fi
+
+fi
