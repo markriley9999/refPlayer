a6311d31d219c0156f6d3d0df2503633b7301630
markriley9999 <markriley9999@gmail.com>
2017-07-21 21:29:06 +0100

    code update sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+CMD=$1
+
+if [ "$CMD" == "update" ]; then
+  echo " - Update code"
+  git pull https://github.com/markriley9999/refPlayer.git master
+  echo " - (Re)install node modules"
+  sudo npm install
+fi
+
 ./electron .
 
