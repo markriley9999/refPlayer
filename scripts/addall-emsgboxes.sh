677ee900503e3715062179b9eacbdf7ea2c7fb60
markriley9999 <markriley9999@gmail.com>
2017-10-03 18:31:08 +0100

    New content

--- scripts/addall-emsgboxes.sh
+++ scripts/addall-emsgboxes.sh
@@ -2,6 +2,29 @@
 
 EMSGNAME=emsg-duk.bin
 
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG1.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG1.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG17.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG17.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG173.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG173.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG330.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG330.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG487.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG487.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG643.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG643.m4s ../content/bbb/$EMSGNAME
+
+./add-emsgbox.sh ../content/sintel/1-130855/SEG800.m4s ../content/bbb/$EMSGNAME
+./add-emsgbox.sh ../content/sintel/ENC-1-136919/SEG800.m4s ../content/bbb/$EMSGNAME
+
+
 ./add-emsgbox.sh ../content/bbb/1-130975/SEG1.m4s ../content/bbb/$EMSGNAME
 ./add-emsgbox.sh ../content/bbb/ENC-6-137036/SEG1.m4s ../content/bbb/$EMSGNAME
 
@@ -23,7 +46,9 @@ EMSGNAME=emsg-duk.bin
 ./add-emsgbox.sh ../content/bbb/1-130975/SEG800.m4s ../content/bbb/$EMSGNAME
 ./add-emsgbox.sh ../content/bbb/ENC-6-137036/SEG800.m4s ../content/bbb/$EMSGNAME
 
+
 ./add-emsgbox.sh ../content/adverts/itv/dash/1-128000/SEG1.m4s ../content/adverts/itv/dash/$EMSGNAME
+
  
 ./add-emsgbox.sh ../content/adverts/ch4/dash/1-128000/SEG1.m4s ../content/adverts/ch4/dash/$EMSGNAME
 
