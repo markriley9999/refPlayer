0f4dbd8ef498e26ad5a1f54703456a76a3aafc6e
markriley9999 <mark.riley@digitaluk.co.uk>
2017-11-28 16:04:20 +0000

    Segtimeline mpds

---
+++ content/segtimeline/copyoveradsegs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 1
+./cpsegs.sh "CLIP2_2/" 4 "2/" 1
+./cpsegs.sh "CLIP2_3/" 4 "3/" 1
+./cpsegs.sh "CLIP2_4/" 4 "4/" 1
+./cpsegs.sh "CLIP2_5/" 4 "5/" 1
+./cpsegs.sh "CLIP2_6/" 4 "6/" 1
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 158
+./cpsegs.sh "CLIP2_2/" 4 "2/" 158
+./cpsegs.sh "CLIP2_3/" 4 "3/" 158
+./cpsegs.sh "CLIP2_4/" 4 "4/" 158
+./cpsegs.sh "CLIP2_5/" 4 "5/" 158
+./cpsegs.sh "CLIP2_6/" 4 "6/" 158
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 315
+./cpsegs.sh "CLIP2_2/" 4 "2/" 315
+./cpsegs.sh "CLIP2_3/" 4 "3/" 315
+./cpsegs.sh "CLIP2_4/" 4 "4/" 315
+./cpsegs.sh "CLIP2_5/" 4 "5/" 315
+./cpsegs.sh "CLIP2_6/" 4 "6/" 315
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 472
+./cpsegs.sh "CLIP2_2/" 4 "2/" 472
+./cpsegs.sh "CLIP2_3/" 4 "3/" 472
+./cpsegs.sh "CLIP2_4/" 4 "4/" 472
+./cpsegs.sh "CLIP2_5/" 4 "5/" 472
+./cpsegs.sh "CLIP2_6/" 4 "6/" 472
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 629
+./cpsegs.sh "CLIP2_2/" 4 "2/" 629
+./cpsegs.sh "CLIP2_3/" 4 "3/" 629
+./cpsegs.sh "CLIP2_4/" 4 "4/" 629
+./cpsegs.sh "CLIP2_5/" 4 "5/" 629
+./cpsegs.sh "CLIP2_6/" 4 "6/" 629
+
+./cpsegs.sh "CLIP2_1/" 4 "1/" 786
+./cpsegs.sh "CLIP2_2/" 4 "2/" 786
+./cpsegs.sh "CLIP2_3/" 4 "3/" 786
+./cpsegs.sh "CLIP2_4/" 4 "4/" 786
+./cpsegs.sh "CLIP2_5/" 4 "5/" 786
+./cpsegs.sh "CLIP2_6/" 4 "6/" 786
+
