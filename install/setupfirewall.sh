6d0c163dcf7e5677fcd7b0e2624fe59b3ab7547a
markriley9999 <mark.riley@digitaluk.co.uk>
2018-02-01 15:39:25 +0000

    Added port 443 - untested

--- install/setupfirewall.sh
+++ install/setupfirewall.sh
@@ -2,3 +2,8 @@ sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-po
 sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
 
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8082
+sudo iptables -A INPUT -p tcp -m tcp --sport 443 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+
