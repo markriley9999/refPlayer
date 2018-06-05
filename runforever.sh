1c8831921faf1399153ea2d60d2743c1969972dd
markriley9999 <ec2-user@ip-172-31-1-149.eu-west-2.compute.internal>
2018-06-05 13:57:09 +0000

    append log option

--- runforever.sh
+++ runforever.sh
@@ -5,6 +5,6 @@
 mkdir -p runlog
 
 ./forever stopall
-./forever start -p $(pwd) -d --minUptime 1000 --spinSleepTime=1000 -a -l runlog/forever.log -o runlog/out.log -e runlog/err.log index.js
+./forever start -p $(pwd) -d -a --minUptime 1000 --spinSleepTime=1000 -a -l runlog/forever.log -o runlog/out.log -e runlog/err.log index.js
 ./forever list
 
