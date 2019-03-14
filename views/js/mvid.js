ebcd7e2d4f5ac654df0f0da9adb7292a2b7e78ba
markriley9999 <mark.riley@digitaluk.co.uk>
2019-03-14 12:40:23 +0000

    Update mvid.js

--- views/js/mvid.js
+++ views/js/mvid.js
@@ -106,8 +106,8 @@ mVid.start = function () {
                     srch = "?noreload=1";
                 }
                 
-                window.location.reload(true);
-                window.location.replace(window.location.href + srch);
+                //window.location.reload(true);
+                window.location.assign(window.location.href + srch);
                 //window.location.reload(true);
             }
         }, 5000
